e5f1496fcbeea4c6864eff147cdf566b65c45392
Markus Köhler <info@koehler.pro>
2017-12-06 18:34:01 +0100

    DO Spaces config

--- config/filesystems.php
+++ config/filesystems.php
@@ -26,7 +26,7 @@ return [
     |
     */
 
-    'cloud' => env('FILESYSTEM_CLOUD', 's3'),
+    'cloud' => env('FILESYSTEM_CLOUD', 'spaces'),
 
     /*
     |--------------------------------------------------------------------------
@@ -54,6 +54,15 @@ return [
             'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
         ],
+        'spaces' => [
+	        'driver' => 's3',
+	        'key' => env('DO_SPACES_KEY'),
+	        'secret' => env('DO_SPACES_SECRET'),
+	        'endpoint' => env('DO_SPACES_ENDPOINT'),
+	        'region' => env('DO_SPACES_REGION'),
+	        'bucket' => env('DO_SPACES_BUCKET'),
+        ],
+
 
         's3' => [
             'driver' => 's3',
