b5d191bb2e74110a735faf25939479b96d3e4708
Markus Köhler <info@koehler.pro>
2017-12-06 18:34:48 +0100

    migration for internal receipts table

---
+++ database/migrations/2017_12_04_213009_create_internal_receipts_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateInternalReceiptsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('internal_receipts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('serial_no');
+            $table->string('creditor_name');
+            $table->string('creditor_address_1');
+            $table->string('creditor_address_2');
+            $table->string('creditor_place');
+            $table->string('expenditure_type');
+            $table->date('expenditure_date');
+            $table->unsignedDecimal('expenditure_costs');
+            $table->string('reason');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('internal_receipts');
+    }
+}
