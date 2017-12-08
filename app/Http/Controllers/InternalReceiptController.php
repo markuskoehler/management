41426f648963d6d2a6ba755893eedd1d252e7c6b
Markus Köhler <info@koehler.pro>
2017-12-08 16:04:24 +0100

    the Internal Receipt Controller

---
+++ app/Http/Controllers/InternalReceiptController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\InternalReceipt;
+use Illuminate\Http\Request;
+
+class InternalReceiptController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('internalreceipt.index', ['internalreceipts' => InternalReceipt::all()]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\InternalReceipt  $internalReceipt
+     * @return \Illuminate\Http\Response
+     */
+    public function show(InternalReceipt $internalReceipt)
+    {
+        return view('internalreceipt.show', ['internalreceipt' => $internalReceipt]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\InternalReceipt  $internalReceipt
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(InternalReceipt $internalReceipt)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\InternalReceipt  $internalReceipt
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, InternalReceipt $internalReceipt)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\InternalReceipt  $internalReceipt
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(InternalReceipt $internalReceipt)
+    {
+        //
+    }
+}
