01984c56b0dfcf1cc9f4808fed42666fed7ffa80
Markus Koehler <info@koehler.pro>
2017-12-14 16:36:25 +0100

    getting creditors (suppliers) from billomat api

---
+++ app/Markuskoehler/Billomat/Creditors.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\Markuskoehler\Billomat;
+
+class Creditors {
+    public function get()
+    {
+        //return app(Request::class)->get('incomings', null, ['to' => '2017-10-30', 'from' => '2014-01-01'], ['status','supplier']);
+        //return app(Request::class)->get('incomings', null, ['to' => '2017-10-30', 'from' => '2014-01-01']);
+        //return app(Request::class)->get('incomings', 156025, [], [], ['pdf']);
+        return app(Request::class)->get('suppliers')->suppliers->supplier;
+        //return app(Request::class)->get('incomings', 156025, ['format' => 'pdf'], [], ['pdf']);
+    }
+}
