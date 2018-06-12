543ef328d560faa436df6739cb2bc18be1cdb9fd
Markus Koehler <info@koehler.pro>
2018-06-12 10:47:02 +0200

    work in progress

--- app/Console/Commands/BackupsMaintenanceCommand.php
+++ app/Console/Commands/BackupsMaintenanceCommand.php
@@ -38,9 +38,45 @@ class BackupsMaintenanceCommand extends Command
      */
     public function handle()
     {
-        foreach (Storage::disk('storagebox')->allFiles('/dump/') as $file) {
-            if(strpos($file, '.log') === false) $this->info($file);
+        // measure time
+        $start = microtime(true);
+
+        $dir = 'dump/'; // root is ''
+        $backups = [];
+        $file_count = 0;
+
+        foreach (Storage::disk('storagebox')->allFiles('/' . $dir) as $file) {
+            if(strpos($file, '.log') === false) {
+                // get size
+                //$size = Storage::disk('storagebox')->size($file) / 1024 / 1024;
+                $original_file = $file;
+                // remove dir and common prefix
+                $file = str_replace($dir . 'vzdump', '', $file);
+                // replace dashes and underscores
+                $file = str_replace(['-', '_'], ' ', $file);
+                // parse ending
+                $file_parts = explode('.', ltrim($file));
+                $name = $file_parts[0];
+                $ending = $file_parts[1] . '.' . $file_parts[2];
+                // split name parts
+                $name_parts = explode(' ', $name);
+                $type = $name_parts[0];
+                $vmid = $name_parts[1];
+                $date = $name_parts[2] . '-' . $name_parts[3] . '-' . $name_parts[4];
+                $time = $name_parts[5] . ':' . $name_parts[6] . ':' . $name_parts[7];
+
+                $backups[$vmid][$date] = $original_file;
+
+                //$this->info($ending . ' ' . $type . ' ' . $vmid . ' ' . $date . ' ' . $time);
+
+                $file_count++;
+            }
             // todo get size, order by size, index by date, check consistency, delete by size/date/broken
         }
+        dd($backups, $file_count);
+
+        // output measured time
+        $time_elapsed_secs = microtime(true) - $start;
+        $this->info('Execution time: ' . round($time_elapsed_secs, 3) . ' sec');
     }
 }
